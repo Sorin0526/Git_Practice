0bddae4d2f5871b3eb6429418a205d00b4a46ae8
Sorin <sorinmoldovan0526@gmail.com>
2022-01-06 14:37:10 -0600

    First commmit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+/*
+to ignore files you don't want to push to git
+copy the file and pae
+ */
+        System.out.println("First");
+    }
+}
